bcf6f3cc32a34ccf449cb13d1642c317ad59f4ae
Hiksang <goo9801@gmail.com>
2025-11-04 12:02:55 +0900

    meta

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,10 +1,3 @@
-import { NextResponse } from "next/server";
-import { minikitConfig } from "../../../../minikit.config";
-
-export async function GET() {
-  return NextResponse.json(minikitConfig);
-}
-
 import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
